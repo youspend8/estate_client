9cb894b873b82f0c38f10e368fa148bcad047511
Imchaehoon <youspend8@gmail.com>
2020-06-23 18:10:19 +0900

    Request URL Global 변수로 수정

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import SearchBox from './SearchBox';
 import SearchTable from './SearchTable';
 import Axios from 'axios';
 import { TradeContext } from './context/useTradeContext';
+import { REQUEST_URL } from './common/Constant';
 
 const App = () => {
   const [ data, setData ] = useState([]);
   
   const search = async() => {
+    console.log()
     console.log(searchQuery)
-    const response = await Axios.get('http://localhost:8080/search/');
+    const response = await Axios.get(`${REQUEST_URL}/search/`);
     const result = await response.data;
 
     setData(response.data)
