6a3b65d2d8b2c3369b83e2d1d8663c9f90e46857
chaehun <youspend8@gmail.com>
2020-07-10 17:53:50 +0900

    Rolling 항목 추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, createContext } from 'react';
+import React, { useState, useEffect, createContext, useRef } from 'react';
 import SearchBox from '../SearchBox';
 import SearchTable from '../SearchTable';
 import Axios from 'axios';
@@ -8,6 +8,7 @@ import Chart from '../component/chart/Chart';
 import AggregationTable from '../AggregationTable';
 import {withRouter} from 'next/router';
 import Header from '../Header';
+import Label from '../component/atoms/label/Label';
 
 const Index = props => {
   const [ data, setData ] = useState([]);
@@ -93,6 +94,40 @@ const Index = props => {
     stats();
   }, [ sigungu ]);
 
+  const rollLeftRef = useRef();
+  const rollRightRef = useRef();
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const top = Number.parseInt(rollLeftRef.current.style.top.replace('px', ''));
+      // if (ref.current.style.visibility === 'hidden') {
+      //   ref.current.style.visibility = 'visible';
+      // }
+      if (top <= (-29 * 19)) {
+        // ref.current.style.visibility = 'hidden';
+        rollLeftRef.current.style.top = '0px';
+      } else {
+        rollLeftRef.current.style.top = (top - 29) + 'px'; 
+      }
+      rollLeftRef.current.style.transition = 'all 1s ease 0s';
+    }, 3000);
+    return () => clearInterval(timer);
+  }, [ ])
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const top = Number.parseInt(rollRightRef.current.style.top.replace('px', ''));
+
+      if (top <= (-29 * 19)) {
+        rollRightRef.current.style.top = '0px';
+      } else {
+        rollRightRef.current.style.top = (top - 29) + 'px'; 
+      }
+      rollRightRef.current.style.transition = 'all 1s ease 0s';
+    }, 3000);
+    return () => clearInterval(timer);
+  }, [ ]);
+
   return (
     <TradeContext.Provider value={{
       searchQuery: searchQuery,
@@ -103,6 +138,75 @@ const Index = props => {
       <div className="page-wrapper">
         <SearchBox />
         <div style={{margin: '20px 0'}}></div>
+        <div style={{display: 'flex'}}>
+          <div style={{
+            flexGrow: 1,
+            height: '29px',
+            overflow: 'hidden'
+          }}>
+            <div ref={rollLeftRef} style={{
+              display: 'flex', 
+              flexDirection: 'column', 
+              position: 'relative',
+              top: '0px'
+            }}>
+              <div><Label isBold={true}>1.</Label><Label>530000		한남더힐</Label></div>
+              <div><Label isBold={true}>2.</Label><Label>527000		한남더힐</Label></div>
+              <div><Label isBold={true}>3.</Label><Label>493000		한남더힐</Label></div>
+              <div><Label isBold={true}>4.</Label><Label>480000		타워팰리스2</Label></div>
+              <div><Label isBold={true}>5.</Label><Label>480000		현대6차(78~81,83,84,86,87동)</Label></div>
+              <div><Label isBold={true}>6.</Label><Label>475000		래미안퍼스티지</Label></div>
+              <div><Label isBold={true}>7.</Label><Label>475000		한양8(81동한개동)</Label></div>
+              <div><Label isBold={true}>8.</Label><Label>463000		현대7차(73~77,82,85동)</Label></div>
+              <div><Label isBold={true}>9.</Label><Label>450000		로덴하우스 웨스트빌리지</Label></div>
+              <div><Label isBold={true}>10.</Label><Label>430000		도곡렉슬</Label></div>
+              <div><Label isBold={true}>11.</Label><Label>430000		갤러리아 포레</Label></div>
+              <div><Label isBold={true}>12.</Label><Label>423000		반포 주공1단지</Label></div>
+              <div><Label isBold={true}>13.</Label><Label>400000		아이파크</Label></div>
+              <div><Label isBold={true}>14.</Label><Label>380000		래미안퍼스티지</Label></div>
+              <div><Label isBold={true}>15.</Label><Label>379000		래미안퍼스티지</Label></div>
+              <div><Label isBold={true}>16.</Label><Label>379000		개포우성2</Label></div>
+              <div><Label isBold={true}>17.</Label><Label>375000		갤러리아 포레</Label></div>
+              <div><Label isBold={true}>18.</Label><Label>370000		한보미도맨션1</Label></div>
+              <div><Label isBold={true}>19.</Label><Label>370000		신현대9차</Label></div>
+              <div><Label isBold={true}>20.</Label><Label>370000		현대2차(10,11,20,23,24,25동)</Label></div>
+            </div>
+          </div>
+          
+          <div style={{
+            flexGrow: 1,
+            height: '29px',
+            overflow: 'hidden'
+          }}>
+            <div ref={rollRightRef} style={{
+              display: 'flex', 
+              flexDirection: 'column', 
+              position: 'relative',
+              top: '0px'
+            }}>
+              <div><Label isBold={true}>1.</Label><Label>176460	강남구</Label></div>
+              <div><Label isBold={true}>2.</Label><Label>161677	서초구</Label></div>
+              <div><Label isBold={true}>3.</Label><Label>155074	용산구</Label></div>
+              <div><Label isBold={true}>4.</Label><Label>135965	송파구</Label></div>
+              <div><Label isBold={true}>5.</Label><Label>132297	과천시</Label></div>
+              <div><Label isBold={true}>6.</Label><Label>107043	성동구</Label></div>
+              <div><Label isBold={true}>7.</Label><Label>101118	광진구</Label></div>
+              <div><Label isBold={true}>8.</Label><Label>100502	마포구</Label></div>
+              <div><Label isBold={true}>9.</Label><Label>90480 동작구</Label></div>
+              <div><Label isBold={true}>10.</Label><Label>90035	양천구</Label></div>
+              <div><Label isBold={true}>11.</Label><Label>89980	영등포구</Label></div>
+              <div><Label isBold={true}>12.</Label><Label>89391	분당구</Label></div>
+              <div><Label isBold={true}>13.</Label><Label>88885	중구</Label></div>
+              <div><Label isBold={true}>14.</Label><Label>86143	강동구</Label></div>
+              <div><Label isBold={true}>15.</Label><Label>78788	종로구</Label></div>
+              <div><Label isBold={true}>16.</Label><Label>76181	서대문구</Label></div>
+              <div><Label isBold={true}>17.</Label><Label>73542	수정구</Label></div>
+              <div><Label isBold={true}>18.</Label><Label>70859	동대문구</Label></div>
+              <div><Label isBold={true}>19.</Label><Label>70722	하남시</Label></div>
+              <div><Label isBold={true}>20.</Label><Label>65318	관악구</Label></div>
+            </div>
+          </div>
+        </div>
         <h2 style={{textAlign: 'center', fontWeight: 'bold'}}>지역별 평당가격</h2>
         <Chart data={statsData} />
         <AggregationTable data={statsData} />
